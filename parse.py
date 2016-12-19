5ea16790f9ff98741880880ed5bb85dbcdf177d8
dsn <dodsonn@gmail.com>
2016-12-19 00:17:35 -0500

    cleaned parser

--- parse.py
+++ parse.py
@@ -10,29 +10,22 @@ signs = ['aries','taurus','gemini','cancer','leo','virgo','libra','scorpio','sag
 states = {x:{} for x in signs}
 csvfile = sys.argv[-1]
 
-
 def triples(s):
   for i in xrange(len(s)-2):
     yield (s[i],s[i+1],s[i+2])
 
-#yeah this is a lazy check. don't pass bad files
-if csvfile[-3:] != 'csv':
-  print 'not a valid file'
-else:
-  with open(csvfile) as infile:
-    for line in infile:
-      s = line.split('|') #0 is num, 1 is horo, 2 is date, 3 is sign
-      sign = s[3].rstrip()
-      horo = s[1].rstrip()
-      horo = horo.split() 
-      horo.insert(0,'_S_')
-      horo.append('_E_')
-      for w1,w2,w3 in triples(horo):
-	k = (w1,w2)
-	if k in states[sign]:
-	  states[sign][k].append(w3)
-	else:
-	  states[sign][k] = [w3]
-  #print states
-  with open('states.txt','wb') as outfile:
-    pickle.dump(states,outfile)
+with open(csvfile) as infile:
+  for line in infile:
+    s = line.split('|') #0 is num, 1 is horo, 2 is date, 3 is sign
+    sign = s[3].rstrip()
+    horo = s[1].rstrip().split()
+    horo.insert(0,'_S_')
+    horo.append('_E_')
+    for w1,w2,w3 in triples(horo):
+      k = (w1,w2)
+      if k in states[sign]:
+	states[sign][k].append(w3)
+      else:
+	states[sign][k] = [w3]
+with open('states.txt','wb') as outfile:
+  pickle.dump(states,outfile)
