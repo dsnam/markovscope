c7a3cd21f9d6a7e457c02c35b17042e4c1fb90a1
unknown <dodsonn@gmail.com>
2017-11-09 20:09:57 -0500

    fixes to regex

--- preproc_rnn.py
+++ preproc_rnn.py
@@ -4,12 +4,12 @@ import re
 #from keras.preprocessing.text import Tokenizer
 
 def get_sequences(s,length):
-  for i in xrange(len(s)-length):
-    yield [s[i+j] for j in xrange(length)]
+  for i in range(len(s)-length):
+    yield [s[i+j] for j in range(length)]
 
 def process(seq_length):
   df = pd.read_csv('data/horoscopes.csv',sep='|',names=['ind','horo','date','sign'])
-  patterns = [("’s"," 's"),("’ve"," 've"),("n’t"," n't"),("’re"," 're"),("’d"," 'd"),("’ll"," 'll"),(","," , "),("!"," ! "),("\("," \( "),("\)"," \) "),("\?"," \? "),("\."," . "),("\s{2,}"," ")]
+  patterns = [("’s"," 's"),("’ve"," 've"),("n’t"," n't"),("’re"," 're"),("’d"," 'd"),("’ll"," 'll"),(","," , "),("!"," ! "),("\("," ( "),("\)"," ) "),("\?"," ? "),("\."," . "),(":"," : "),('"',' " ')]
   sequences = []
   words = set([])
   next_words = []
@@ -17,20 +17,20 @@ def process(seq_length):
     s = row['horo']
     if type(s) == type('.'):
       for p in patterns:
-	s = re.sub(p[0],p[1],s)
+        s = re.sub(p[0],p[1],s)
       for seq in get_sequences(s.lower().split(),seq_length):
-	sequences.append(seq[:-1])
-	next_words.append(seq[-1])
-      words = words.union(set(s.lower().split()))
+        sequences.append(seq[:-1])
+        next_words.append(seq[-1])
+        words = words.union(set(s.lower().split()))
       
-  print 'words: ',len(words)
-  print 'seqs: ',len(sequences)
+  print('words: ',len(words))
+  print('seqs: ',len(sequences))
   word_to_idx = dict((c,i) for i,c in enumerate(words))
   idx_to_word = dict((i,c) for i,c in enumerate(words))
-  print 'seq length',seq_length
+  print('seq length',seq_length)
   
-  for i in xrange(len(sequences)):
-    for j in xrange(len(sequences[i])):
+  for i in range(len(sequences)):
+    for j in range(len(sequences[i])):
       sequences[i][j] = word_to_idx[sequences[i][j]]
     next_words[i] = word_to_idx[next_words[i]]
 
